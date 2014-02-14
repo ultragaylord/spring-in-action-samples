e352c98c99dd50c13c3750bc925052fca19f350c
ultragaylord <vargas.gail@gmail.com>
2014-02-14 15:07:32 +0800

    02-14-2014 Added the examples for prototype scope bean and singeton bean tests.

--- SpringInAction/src/main/java/com/nijiiro/ufo/springidol/Stage.java
+++ SpringInAction/src/main/java/com/nijiiro/ufo/springidol/Stage.java
@@ -1,15 +1,46 @@
 package com.nijiiro.ufo.springidol;
 
+/**
+ * A Singleton class which provides a single Stage instance.
+ * @author gvargas.local
+ *
+ */
 public class Stage {
 	
+	private double randomNumber;
+	
+	/**
+	 * sets to private to maintain centralize creation.
+	 */
 	private Stage(){
+		//just set a sample distinction for each instance.
+		setRandomNumber(Math.random());
 		System.out.println("You're in a single stage... ");
-	}
 		
-		private static class StageSingletonHolder {
+	}
+	
+	/**
+	 * Inner class which provides the single Stage Class creation.
+	 * @author gvargas.local
+	 *
+	 */
+	private static class StageSingletonHolder {
 		static Stage instance = new Stage();
 	}
+	
+	/**
+	 * public method which dispenses a singleton instance.
+	 * @return
+	 */
 	public static Stage getInstance(){
 		return StageSingletonHolder.instance;
+	}
+
+	public double getRandomNumber() {
+		return randomNumber;
+	}
+
+	public void setRandomNumber(double randomNumber) {
+		this.randomNumber = randomNumber;
 	}
 }
--- SpringInAction/src/test/java/com/nijiiro/ufo/test/SpringIdolTest.java
+++ SpringInAction/src/test/java/com/nijiiro/ufo/test/SpringIdolTest.java
@@ -2,6 +2,8 @@ package com.nijiiro.ufo.test;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -9,6 +11,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.nijiiro.ufo.springidol.Performer;
 import com.nijiiro.ufo.springidol.Stage;
+import com.nijiiro.ufo.springidol.Ticket;
 
 public class SpringIdolTest {
 	
@@ -19,24 +22,74 @@ public class SpringIdolTest {
 		context = new ClassPathXmlApplicationContext("spring-idol.xml");
 	}
 	
+	/**
+	 * Test for plain constructor-arg example.
+	 */
 	@Test
 	public void performTest() {
 		Performer duke = (Performer) context.getBean("duke");
 		duke.perform();
 	}
-	
+	/**
+	 * Test for constructor-arg example with reference bean definition 
+	 */
 	@Test 
 	public void poeticJugglerTest(){
 		Performer poeticDuke = (Performer) context.getBean("poeticDuke");
 		poeticDuke.perform();
 	}
 	
+	/**
+	 * Test for the singleton bean stage.
+	 * Proves that we are getting the same instance every time this stage bean is called.
+	 */
 	@Test
 	public void singletonBeanTest(){
 		Stage firstStage = (Stage) context.getBean("theStage");
 		Stage secondStage = (Stage) context.getBean("theStage");
+		
 		//Note that the println is called only once even though we get the bean twice.
 		assertEquals(firstStage,secondStage);
+		//These numbers will generate the same instance although the method called is a random double
+		System.out.println(firstStage.getRandomNumber());
+		//will be exactly same as the firststage.
+		System.out.println(secondStage.getRandomNumber());
+		
+		assertNotNull(firstStage.getRandomNumber());
 	}
+	
+	/**
+	 * Test for prototype bean scope.
+	 * If bean scope is default (singleton)
+	 * myTicket will be same as yourTicket.
+	 * 
+	 * 
+	 */
+	@Test
+	public void protoTypeTest(){
+		//This type of tickets are define as default (singeton bean) in bean definitions.
+		Ticket myTicket = (Ticket) context.getBean("singletonTicket");
+		Ticket yourTicket = (Ticket) context.getBean("singletonTicket");
+		
+		
+		System.out.println("This is my singleton ticket :" +myTicket.getTicketNumber());
+		System.out.println("This is your singleton ticket :" +yourTicket.getTicketNumber());
+		//This assertion will be true for bean singletonTicket
+		assertEquals(new BigDecimal(myTicket.getTicketNumber()), new BigDecimal(yourTicket.getTicketNumber()));
+		
+		//This type of tickets are define as default (singeton bean) in bean definitions.
+		Ticket myUniqueTicket = (Ticket) context.getBean("uniqueTicket");
+		Ticket yourUniqueTicket = (Ticket) context.getBean("uniqueTicket");
+		
+
+		System.out.println("This is my unique ticket :" +myUniqueTicket.getTicketNumber());
+		//This will now have a different number since it is define as prototype.
+		System.out.println("This is your unique ticket :" +yourUniqueTicket.getTicketNumber());
+		//This assertion will be true for bean uniqueTicket defined as scope=prototype.
+		assertNotEquals(new BigDecimal(myUniqueTicket.getTicketNumber()), new BigDecimal(yourUniqueTicket.getTicketNumber()));
+		
+	}
+	
+	
 	
 }
